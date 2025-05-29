1b43bbc0b50543b081d82ac625861b7073847318
ramanraj00 <r02519625@gmail.com>
2025-05-29 15:38:47 +0530

    datatypes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version 
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) //code readability should be high 
+
+console.log("Raman")
+
+let name = "Raman"
+let age = 18
+let isLoggedIn = false 
+let state;
+
+
+// Primitive Data Type
+//bigint => for big values
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//Object
+
+console.log(typeof age);
+console.log(typeof "Raman");
+
+console.log(typeof null); // output => Object
+console.log(typeof undefined) // output => undefined
