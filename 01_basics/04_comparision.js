d709ff41c0cabe4232c39de7881cfd5c842a8596
ramanraj00 <r02519625@gmail.com>
2025-05-30 00:58:10 +0530

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0 );
+console.log(null == 0);
+console.log(null >= 0); // = and > together treat null as 0 thats why output is true
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
