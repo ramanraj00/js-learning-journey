9e3f3e742314dce67231e014941d7ed2094a6d75
ramanraj00 <r02519625@gmail.com>
2025-05-29 16:28:03 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Raman"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NAN
+// true => 1; false=> 0
+
+// let isLoggedIn = 1
+
+// let booLeanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booLeanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Raman" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber );
