057c4b8cbd7c20d642198206ef24557a456f4d09
ramanraj00 <r02519625@gmail.com>
2025-06-01 18:05:12 +0530

    Object

---
+++ 02_basics/03_object.js
@@ -0,0 +1,41 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Raman" ,
+    "full name": "Raman Kashyap",
+    [mySym]: "mykey1",
+    age : 18 , 
+    location: "Noida",
+    email:"raman@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "Ramanraj@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "Raman@microsoft.com"
+console.log(JsUser);
+
+JsUser.greeting= function(){
+    console.log("Hello Js User");
+}
+
+JsUser.greetingTwo= function(){
+    console.log(`Hello Js User, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+//
