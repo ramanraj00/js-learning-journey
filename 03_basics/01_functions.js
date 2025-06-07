5b8472cb34ecfa25bf04ab848d6d062d7e2df4f1
ramanraj00 <r02519625@gmail.com>
2025-06-07 23:35:50 +0530

    Function Done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,72 +1,97 @@
 
 
-function sayMyName (){
-console.log("R");
-console.log("A");
-console.log("M");
-console.log("A");
-console.log("N");
+// function sayMyName (){
+// console.log("R");
+// console.log("A");
+// console.log("M");
+// console.log("A");
+// console.log("N");
 
-}
+// }
 
-sayMyName()
+// sayMyName()
 
-function addTwoNumbers(number1, number2) {
+// function addTwoNumbers(number1, number2) {
 
-    // let result = number1 + number2
-    // return result
+//     // let result = number1 + number2
+//     // return result
 
-    return number1 + number2
+//     return number1 + number2
 
     
-}
+// }
 
-  const result = addTwoNumbers(3 , 5)
+//   const result = addTwoNumbers(3 , 5)
 
-  // console.log("Result:" ,  result)
+//   // console.log("Result:" ,  result)
 
-  function loginUserMessage(username = "Ramon") {
-    if(username === undefined){
-        console.log("Please enter a username")
-        return
-    }
-    return `${username} just logged in `
-}
+//   function loginUserMessage(username = "Ramon") {
+//     if(username === undefined){
+//         console.log("Please enter a username")
+//         return
+//     }
+//     return `${username} just logged in `
+// }
 
- //console.log(loginUserMessage ("hitesh"))
-//  console.log(loginUserMessage("Hitesh"))
+//  //console.log(loginUserMessage ("hitesh"))
+// //  console.log(loginUserMessage("Hitesh"))
 
 
- function calculatecartPrice(...num1) {
-    return num1
- }
+//  function calculatecartPrice(...num1) {
+//     return num1
+//  }
 
- // console.log (calculatecartPrice (200 , 400 , 500 , 2000));
+//  // console.log (calculatecartPrice (200 , 400 , 500 , 2000));
 
- const user = {
-    username: "hitesh",
-    price: 199
- }
+//  const user = {
+//     username: "hitesh",
+//     price: 199
+//  }
 
- function handleObject (anyobject) {
-    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+//  function handleObject (anyobject) {
+//     // console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
 
- }
+//  }
 
 //  handleObject(user)
 
-handleObject ({
-    username : "Sam",
-    price : 399
+// handleObject ({
+//     username : "Sam",
+//     price : 399
 
-})
+// })
 
-const myNewArrray = [200 , 400 , 100 , 600]
+// const myNewArrray = [200 , 400 , 100 , 600]
 
-function returnSecondValue(getArray){
-    return getArray[1]
+// function returnSecondValue(getArray){
+//     return getArray[1]
 
-}
+// }
 
 // console.log(returnSecondValue(myNewArrray));
-console.log(returnSecondValue([200 ,400 , 500, 1000 ]));
+// console.log(returnSecondValue([200 ,400 , 500, 1000 ]));
+
+
+// function loginUserMessage (Username) {
+//     if(Username=== undefined){
+//         console.log("Username daal na bhosdike");
+//        return
+//     }
+//     return `${Username} just loged in`
+// }
+
+// const answer = loginUserMessage()
+// console.log (answer);
+
+// const luser = {
+// }
+
+function handleObject(anyObject){
+    console.log(`Usename is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+handleObject ({
+    username : "Sam" ,
+    price: 399
+})
+
